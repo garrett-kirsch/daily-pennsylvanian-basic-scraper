5f71c2d30b1d23799c9bcb6b6da817fef5791c02
Garrett Kirsch <gkirsch@seas.upenn.edu>
2025-02-25 19:42:43 -0500

    debug

--- script.py
+++ script.py
@@ -28,7 +28,6 @@ def scrape_data():
     loguru.logger.info(f"Request status code: {req.status_code}")
 
     data = {}
-    return_data = false
 
     if req.ok:
         soup = bs4.BeautifulSoup(req.text, "html.parser")
@@ -52,12 +51,12 @@ def scrape_data():
             data["top_news_headline"] = "" if top_news_headline is None else top_news_headline.text
 
         # top opinion headline
-        # opinion_header = soup.find("h3", class_="frontpage-section", string="Opinion")
-        # if opinion_header:
-        #     top_opinion_article = opinion_header.find_next("div", class_="article-summary")
-        #     if top_opinion_article:
-        #         top_opinion_headline = top_opinion_article.find("a", class_="frontpage-link medium-link font-regular")
-        #         data["top_opinion_headline"] = "" if top_opinion_headline is None else top_opinion_headline.text
+        opinion_header = soup.find("h3", class_="frontpage-section", string="Opinion")
+        if opinion_header:
+            top_opinion_article = opinion_header.find_next("div", class_="article-summary")
+            if top_opinion_article:
+                top_opinion_headline = top_opinion_article.find("a", class_="frontpage-link medium-link font-regular")
+                data["top_opinion_headline"] = "" if top_opinion_headline is None else top_opinion_headline.text
         
         # top sports headline
         # sports section header
